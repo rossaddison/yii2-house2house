f13b4fd401204a53da8315fe9c0ffabc722402d2
Ross Addison <rossaddison@users.noreply.github.com>
2019-12-26 22:09:37 +0000

    Create params.php

---
+++ frontend/config/params.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'adminEmail' => 'email@domain.co.uk',
+    'supportEmail' => 'supportemail@domain.co.uk',
+    'user.passwordResetTokenExpire' => 3600,
+    'icon-framework' => 'fa',
+    'bsDependencyEnabled' => true, 
+    'bsVersion' => '4.x',
+    'portalMode'=>'non-maintenance',
+];
