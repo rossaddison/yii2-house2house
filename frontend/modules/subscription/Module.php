895adfc719a9665e69b7a5a7d33fb6b368a137d3
Ross Addison <rossaddison@users.noreply.github.com>
2019-12-27 11:38:35 +0000

    Create Module.php

---
+++ frontend/modules/subscription/Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace frontend\modules\subscription;
+
+/**
+ * subscription module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'frontend\modules\subscription\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
