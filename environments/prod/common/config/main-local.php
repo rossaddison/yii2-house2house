eef163f4d9179958284e1452148e153ec91fdcaf
Ross Addison <rossaddison@users.noreply.github.com>
2019-12-28 15:52:15 +0000

    Add files via upload

---
+++ environments/prod/common/config/main-local.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=ra_windowcleaning_co_uk',
+            'username' => 'ra_windowcleaning_co_uk',
+            'password' => 'jump22it',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+        ],
+    ],
+];
