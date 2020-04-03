d02044782c35d5286f8a4b75dc73be7394737aa8
Ross Addison <rossaddison@users.noreply.github.com>
2020-04-03 12:17:47 +0100

    Add files via upload

---
+++ no_subscription_index.php
@@ -0,0 +1,46 @@
+<?php
+
+//if (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] !== 'on') {
+    //if(!headers_sent()) {
+        //header("Status: 301 Moved Permanently");
+       // header(sprintf(
+            //'Location: https://%s%s',
+            //$_SERVER['HTTP_HOST'],
+            //$_SERVER['REQUEST_URI']
+        //));
+        //exit();
+    //}
+//}
+
+
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
+defined('YII_CONSOLE') or define('YII_CONSOLE', false);
+
+//vendors from composer.json
+require (__DIR__ . '/vendor/autoload.php');
+
+//yii framework
+require(__DIR__  . '/vendor/yiisoft/yii2/Yii.php');
+
+//aliases
+require(__DIR__ . '/common/config/bootstrap.php');
+
+//empty
+require(__DIR__ . '/frontend/config/bootstrap.php');
+
+$config = yii\helpers\ArrayHelper::merge(
+    //filecache and dbmanager
+    require(__DIR__ . '/common/config/main.php'),
+    
+    //db component,  
+    require(__DIR__ . '/common/config/main-local.php'),
+    
+    //etc... UrlManager  swiftmailer
+    require(__DIR__ . '/frontend/config/main.php'),
+    
+    //cookie validation key
+    require(__DIR__ . '/frontend/config/main-local.php')
+);
+
+(new yii\web\Application($config))->run();
