f55dc69812d7cede69ae20056fb36f365f1195ac
Ross Addison <rossaddison@users.noreply.github.com>
2020-04-07 10:36:31 +0100

    Update main.php

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -2,7 +2,6 @@
 use yii\helpers\Html;
 use yii\bootstrap4\Nav;
 use yii\bootstrap4\NavBar;
-//use yii\widgets\Breadcrumbs;
 use yii\bootstrap4\Breadcrumbs;
 use frontend\assets\AppAsset;
 use frontend\models\Company;
