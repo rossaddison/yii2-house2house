be0a0296b92da9893a96ee650be514fe10c4684f
Ross Addison <rossaddison@users.noreply.github.com>
2020-04-12 20:23:34 +0100

    Create wamp_kill_and_empty_database_tables_db.bat

---
+++ wamp_kill_and_empty_database_tables_db.bat
@@ -0,0 +1,4 @@
+yii migrate-db-namespaced/down --interactive=1
+yii migrate-db-non-namespaced/down --interactive=1
+yii migrate-db-auth/down --interactive=1
+yii migrate-db-paypal/down --interactive=1
