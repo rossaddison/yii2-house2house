a29bd1c63354a7c2421936acdcfd83a8e9172861
Ross Addison <rossaddison@users.noreply.github.com>
2020-04-13 22:48:33 +0100

    Delete wamp_fill_database_db.bat

--- wamp_fill_database_db.bat
+++
@@ -1,5 +0,0 @@
-yii migrate-db-namespaced --interactive=0
-yii migrate-db-non-namespaced --interactive=0
-yii migrate-db-rbac --interactive=0
-yii migrate-db-rbac-fill --interactive=0
-yii migrate-db-paypal --interactive=0
