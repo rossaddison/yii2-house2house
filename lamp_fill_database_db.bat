2536c733b67ea5ddefb98778aa30bfa65cab4277
Ross Addison <rossaddison@users.noreply.github.com>
2020-04-13 01:35:13 +0100

    Delete lamp_fill_database_db.bat

--- lamp_fill_database_db.bat
+++
@@ -1,5 +0,0 @@
-php yii migrate-db-namespaced --interactive=0
-php yii migrate-db-non-namespaced --interactive=0
-php yii migrate-db-rbac --interactive=0
-php yii migrate-db-rbac-fill --interactive=0
-php yii migrate-db-paypal --interactive=0
