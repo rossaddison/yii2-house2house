6753e504306aa7fd7eb46669425835fa08c6a6ba
Ross Addison <rossaddison@users.noreply.github.com>
2020-04-16 18:27:10 +0100

    Update BackuperController.php

--- frontend/modules/backup/controllers/BackuperController.php
+++ frontend/modules/backup/controllers/BackuperController.php
@@ -6,10 +6,11 @@ use frontend\modules\backup\components\BackupHelper;
 use frontend\modules\backup\components\SessionHelper;
 use frontend\modules\backup\models\FinalStep;
 use frontend\modules\backup\models\BackupModel;
+use frontend\modules\backup\models\DumpModel;
 use Yii;
 use yii\base\DynamicModel;
 use yii\web\Controller;
-
+use Ifsnop\Mysqldump as IMysqldump;
 
 class BackuperController extends Controller
 {
@@ -74,6 +75,41 @@ class BackuperController extends Controller
                 'model' => $model,
             ]
         );
+    }
+    
+    public function actionDump()
+    {
+           $model = new DumpModel(); 
+           try {
+                $database_handle = $model->getDatabasehandle();
+                $database = Yii::$app->$database_handle->createCommand("SELECT DATABASE()")->queryScalar();
+                $dumpit = new IMysqldump\Mysqldump('mysql:host=localhost;dbname='.$database.'', Yii::$app->$database_handle->username, Yii::$app->$database_handle->password);
+                $basepath = \Yii::getAlias('@webroot');
+                $timestamp = time();
+                $model->save_from_path = "/mysqlbackup/".$timestamp."_".Yii::$app->security->generateRandomString()."_".$database_handle;
+                $model->path = $basepath .  $model->save_from_path;
+                //$path = $basepath . "/mysqlbackup/".$database_handle;
+                mkdir($model->path,0777); 
+                if (is_dir($model->path)){$model->created_directory_successfully = true;}
+                else {$model->created_directory_successfully = false;}
+	        $model->path_and_filename = $basepath . $model->save_from_path."/".$timestamp.".sql";
+                $model->save_from_path_and_filename = $model->save_from_path . "/".$timestamp.".sql";
+                $dumpit->start($model->path_and_filename);
+            } catch (\Exception $e) {
+                $model->resultmessage = 'mysqldump-php error: ' . $e->getMessage();
+            }
+            return $this->render('dump',
+                     [
+                       'dumpit'=>$dumpit,
+                       'model' => $model,
+                       'path' => $model->path,
+                       'path_and_filename' => $model->path_and_filename,
+                       'resultmessage'=> $model->resultmessage,  
+                       'created_directory_successfully'=>$model->created_directory_successfully,
+                       'save_from_path'=>$model->save_from_path,
+                     ]
+             );
+            
     }
     
     public function actionFinal()
