878c1f04edcd172698a0dce7266f8061084eb87a
Ross Addison <rossaddison@users.noreply.github.com>
2020-04-17 00:24:22 +0100

    Update index.php

--- frontend/modules/backup/views/backuper/index.php
+++ frontend/modules/backup/views/backuper/index.php
@@ -3,11 +3,11 @@
 use \kartik\icons\Icon;
 use yii\helpers\Url;
 
-$this->title = Yii::t('app', 'Backuper');
+$this->title = Yii::t('app', 'mySql Backup');
 ?>
-<h1>
-    <?= Yii::t('app', 'Backuper') ?>
-</h1>
+<h3>
+    <?= Yii::t('app', 'mySql Backup') ?>
+</h3>
 
 <?php if ($minPhpVersion === false): ?>
     <div class="alert alert-danger">
@@ -27,25 +27,16 @@ $this->title = Yii::t('app', 'Backuper');
 
 <div class="backuper-controls">
     <div class="alert alert-info">
-        <strong><?= Yii::t('app', 'Your PHP version is suitable ie. > 5.5', [PHP_VERSION]) ?></strong>
+        <strong><?= Yii::t('app', 'Your PHP version '.phpversion().' is suitable ie. > 5.5', [PHP_VERSION]) ?></strong>
     </div>
     <div class="alert alert-info">
     <p>
             <?= Yii::t('app', 'Your document root is correctly set to: ') ?>
-            <code><?= realpath(Yii::getAlias('@app/web/')) ?></code>.
+            <code><?= realpath(Yii::getAlias('@webroot')) ?></code>.
     </p>
     </div>
-    <a href="<?= Url::toRoute(['backup']) ?>" class="btn btn-primary btn-lg pull-right ladda-button" data-style="expand-left">
+    <a href="<?= Url::toRoute(['dump']) ?>" class="btn btn-primary btn-lg pull-right ladda-button" data-style="expand-left">
         <?= Yii::t('app', 'Next') ?>
         <?= Icon::show('arrow-right') ?>
     </a>
 </div>
-
-<?php
-   //echo "Docroot is ".$docRoot;
-   //echo "<br>";
-   //echo "Yii request url ".Yii::$app->request->url;
-   //echo "<br>";
-   //echo "strpos " . strpos(Yii::$app->request->url, '/installer');
-   //echo "alias @migrations" .Yii::getAlias('@migrations');
- ?>
