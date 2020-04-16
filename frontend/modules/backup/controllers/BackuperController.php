19cb5c19e9452782e6ff582744d65b6f7c897266
Ross Addison <rossaddison@users.noreply.github.com>
2020-04-17 00:12:04 +0100

    Update BackuperController.php

--- frontend/modules/backup/controllers/BackuperController.php
+++ frontend/modules/backup/controllers/BackuperController.php
@@ -2,18 +2,44 @@
 
 namespace frontend\modules\backup\controllers;
 
-use frontend\modules\backup\components\BackupHelper;
-use frontend\modules\backup\components\SessionHelper;
-use frontend\modules\backup\models\FinalStep;
-use frontend\modules\backup\models\BackupModel;
 use frontend\modules\backup\models\DumpModel;
 use Yii;
-use yii\base\DynamicModel;
 use yii\web\Controller;
+use yii\filters\VerbFilter;
 use Ifsnop\Mysqldump as IMysqldump;
 
 class BackuperController extends Controller
 {
+     public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                   // 'delete' => ['POST'],
+                ],
+            ],
+           'access' => 
+                            [
+                            'class' => \yii\filters\AccessControl::className(),
+                            'only' => ['index','dump'],
+                            'rules' => [
+                            [
+                              'allow' => true,
+                              'roles' => ['admin','support'],
+                            ],
+                            [
+                              'allow' => false,
+                              'roles' => ['?'],
+                            ],  
+                            [
+                              'allow' => true,
+                              'verbs' => ['POST']
+                            ],  
+                            ],
+            ], 
+        ];
+    }
     // set simple layout
     // public $layout = 'backuper';
     private $db = null;
@@ -31,48 +57,6 @@ class BackuperController extends Controller
             [
                 'minPhpVersion' => $minPhpVersion,
                 'docRoot' => $docRoot,
-            ]
-        );
-    }
-    
-    public function actionBackup()
-    {
-        $model = new BackupModel();
-        $sessionHelper = Yii::$app->get('sessionHelper');
-        $model->ignore_time_limit_warning = $sessionHelper->get('ignore_time_limit_warning', false);
-        if ($model->load(Yii::$app->request->post())) {
-            $model->validate();
-            foreach ($model->getAttributes() as $key => $value) {
-                $sessionHelper->set($key, $value);
-            }
-        }
-        
-        $helper = Yii::createObject([
-            'class' => BackupHelper::className(),
-        ]);
-        
-        $process = $helper->applyAppBackupCron();
-
-        $commandToRun = $process->getCommandLine();
-
-        $check = $this->checkTime($model->ignore_time_limit_warning);
-        if (Yii::$app->request->isPost) {
-            if ($check) {
-                $process->run();
-                if ($process->getExitCode() === 0) {
-                        Yii::$app->session->setFlash('info', Yii::t('app', 'Backup completed successfully'));  
-                        return $this->redirect(['final']);
-                }
-            }
-        }
-        
-        return $this->render(
-            'backup',
-            [
-                'check' => $check,
-                'commandToRun' => $commandToRun,
-                'process' => $process,
-                'model' => $model,
             ]
         );
     }
@@ -110,60 +94,5 @@ class BackuperController extends Controller
                      ]
              );
             
-    }
-    
-    public function actionFinal()
-    {
-        $model = new FinalStep();
-        $model->serverName = Yii::$app->request->serverName;
-        
-        if (extension_loaded('memcached') || extension_loaded('memcache')) {
-            $model->cacheClass = 'yii\caching\MemCache';
-            if (extension_loaded('memcached')) {
-                $model->useMemcached = true;
-            }
-        }
-
-        if (Yii::$app->request->serverPort !== 80) {
-            $model->serverPort = Yii::$app->request->serverPort;
-        }
-        
-        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
-
-                return $this->redirect(['complete']);
-        }
-      
-         $cacheClasses = [
-            'yii\caching\FileCache',
-            'yii\caching\MemCache',
-            'yii\caching\XCache',
-            'yii\caching\ZendDataCache',
-            'yii\caching\ApcCache',
-        ];
-
-        return $this->render(
-            'final',
-            [
-                'model' => $model,
-                'cacheClasses' => $cacheClasses,
-            ]
-        );
-    }
-
-    public function actionComplete()
-    {
-        return $this->render(
-            'complete'
-        );
-    }
-    
-    private function checkTime($ignoreTimeLimit=false)
-    {
-        if (BackupHelper::unlimitTime() === false && $ignoreTimeLimit === false) {
-            Yii::$app->session->setFlash('warning', Yii::t('app', 'Can\'t set time limit to 0. Some operations may not complete.'));
-            return false;
-        } else {
-            return true;
-        }
-    }
+     }
 }
