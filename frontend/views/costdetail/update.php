a873f0684f272b0cc5e92b2e02b62ce907c91c81
Ross Addison <rossaddison@users.noreply.github.com>
2020-04-19 17:00:12 +0100

    Update update.php

--- frontend/views/costdetail/update.php
+++ frontend/views/costdetail/update.php
@@ -3,19 +3,19 @@
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-/* @var $model frontend\models\Salesorderdetail */
+/* @var $model frontend\models\costdetail */
 
-$this->title = 'Update Houses to Clean: ID: ' . $model->sales_order_detail_id;
-$this->params['breadcrumbs'][] = ['label' => 'Daily Cleans:', 'url' => ['salesorderheader/index','id'=>$model->sales_order_id]];
-$this->params['breadcrumbs'][] = ['label' => $model->salesOrder->clean_date];
-$this->params['breadcrumbs'][] = ['label' => 'Houses to Clean:', 'url' => ['index','id'=>$model->sales_order_id]];
-$this->params['breadcrumbs'][] = ['label' => $model->productcategory->name. " in ".$model->productcategory->description];
-$this->params['breadcrumbs'][] = ['label' => $model->productsubcategory->name];
-$this->params['breadcrumbs'][] = ['label' => $model->product->productnumber];
+$this->title = 'Update Costs: ID: ' . $model->cost_detail_id;
+$this->params['breadcrumbs'][] = ['label' => 'Daily Costs:', 'url' => ['costheader/index','id'=>$model->cost_header_id]];
+$this->params['breadcrumbs'][] = ['label' => $model->costHeader->cost_date];
+$this->params['breadcrumbs'][] = ['label' => 'Costs on this day:', 'url' => ['index','id'=>$model->cost_header_id]];
+$this->params['breadcrumbs'][] = ['label' => $model->costcategory->name. " in ".$model->costcategory->description];
+$this->params['breadcrumbs'][] = ['label' => $model->costsubcategory->name];
+$this->params['breadcrumbs'][] = ['label' => $model->cost->costnumber];
 //$this->params['breadcrumbs'][] = ['label' => $model->sales_order_detail_id, 'url' => ['view', 'id' => $model->sales_order_detail_id]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
-<div class="salesorderdetail-update">
+<div class="costdetail-update">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
