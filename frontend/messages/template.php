03625ce21abfbad15d428ba251296f4fd53b1493
Ross Addison <rossaddison@users.noreply.github.com>
2020-08-22 21:08:24 +0100

    Bug: Duplicate format
    The default format is 'db' not 'php'.

--- frontend/messages/template.php
+++ frontend/messages/template.php
@@ -41,7 +41,7 @@ return [
     ],
 
     // 'php' output format is for saving messages to php files.
-    'format' => 'php',
+    //'format' => 'php',
     // Root directory containing message translations.
     'messagePath' => __DIR__,
     // boolean, whether the message file should be overwritten with the merged messages
