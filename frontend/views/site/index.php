15b483f886593b6350b37ebb341a497de81821cd
Ross Addison <rossaddison@users.noreply.github.com>
2020-08-22 22:41:19 +0100

    Bugfix: Undefined variable: $last_cust
    If the Product ie. Houses table is empty the Dashboard is now designed not to show.

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,12 +1,13 @@
 <?php
 use frontend\components\Utilities;
+use frontend\models\Product;
 ?>
 <br>
 <div class="site-index">
     <div class="body-content">
         <div class="container">
             <?php
-              if (!Yii::$app->user->isGuest){
+             if ((!Yii::$app->user->isGuest) && (Product::find()->count()) <> 0){
                 Utilities::Home_tabs_service();
               }
             ?>
